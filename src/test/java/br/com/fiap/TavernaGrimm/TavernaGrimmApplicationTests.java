3c5a1d9b6b05e76d14ad1a2ffba316efd1960218
Caepena <caepenafiel@gmail.com>
2025-04-15 10:29:59 -0300

    Criação da estrutura básica do projeto.

---
+++ src/test/java/br/com/fiap/TavernaGrimm/TavernaGrimmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.TavernaGrimm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavernaGrimmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
